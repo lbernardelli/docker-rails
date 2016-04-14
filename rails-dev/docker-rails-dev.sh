dcd033717c15959c4f522354506ff0ea7e26bd96
elifarley <elifarley@gmail.com>
2016-04-14 12:33:43 -0300

    Update docker-rails-dev.sh

--- rails-dev/docker-rails-dev.sh
+++ rails-dev/docker-rails-dev.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -x
 IMAGE="elifarley/docker-rails:rails-dev"
-#docker pull "$IMAGE"
-
-docker rm -f rails-dev
 
 project_root="$1"; shift
-
 test "$project_root" || exec ssh -o StrictHostKeyChecking=no -p2200 app@localhost
+
+docker pull "$IMAGE"
+
+docker rm -f rails-dev
 
 exec docker run --name rails-dev \
 -d \
