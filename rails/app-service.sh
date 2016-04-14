1d86f14a4ea191dbdaedfc66c67ce638b42a7f39
Elifarley C <elifarley@users.noreply.github.com>
2016-04-14 00:53:49 -0300

    Create app-service.sh

---
+++ rails/app-service.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -e
+sv -w7 check 
+
+syslog
+
+-ng
+cd /$_USER
+sleep 1
+[[ -p pipe.log ]] || { echo "${PWD}/pipe.log is not a pipe"; exit 1; }
+exec >pipe.log 2>&1 /sbin/setuser nobody \
+  ./app.sh
