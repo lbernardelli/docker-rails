adbb50217e7fd6c62041d55465010755746441f8
Elifarley C <elifarley@users.noreply.github.com>
2016-04-18 12:49:53 -0300

    Keep separate Bash history file for each project dir

--- rails-dev/docker-rails-dev.sh
+++ rails-dev/docker-rails-dev.sh
@@ -9,11 +9,15 @@ docker pull "$IMAGE"
 
 docker rm -f rails-dev
 
+DDE_BASH_HISTORY=~/.dde/bash-history-"$(basename "$project_root")"
+mkdir -p "$(dirname "$DDE_BASH_HISTORY")" && touch "$DDE_BASH_HISTORY"
+
 exec docker run --name rails-dev \
 -d \
 -p 2200:2200 \
 -v ~/.ssh/id_rsa.pub:/mnt-ssh-config/authorized_keys:ro \
 -v ~/.ssh/id_rsa:/mnt-ssh-config/id_rsa:ro \
 -v ~/.ssh/known_hosts:/mnt-ssh-config/known_hosts:ro \
+-v "$DDE_BASH_HISTORY":/app/.bash_history \
 -v "$project_root":/data \
 "$IMAGE" "$@"
