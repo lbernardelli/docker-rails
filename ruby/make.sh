0791202be39c61c0bb2dd48708294d297c267042
Elifarley C <elifarley@users.noreply.github.com>
2016-05-03 15:11:27 -0300

    Update make.sh (#4)

--- ruby/make.sh
+++ ruby/make.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 CMD_BASE="$(readlink -m $0)" || CMD_BASE="$0"; CMD_BASE="$(dirname $CMD_BASE)"
 
-exec docker build -t elifarley/ruby:2.3.0 "$CMD_BASE"
+exec docker build -t elifarley/docker-rails:ruby-2.3.0 "$CMD_BASE"
