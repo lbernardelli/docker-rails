512da4271acfcc92a712602ae74837895f78470d
Elifarley C <elifarley@users.noreply.github.com>
2016-05-03 15:29:17 -0300

    Fix bad paste

--- rails/app-service.sh
+++ rails/app-service.sh
@@ -1,11 +1,8 @@
 #!/bin/bash -e
-sv -w7 check 
-
-syslog
-
--ng
+sv -w7 check syslog-ng
 cd /$_USER
 sleep 1
 [[ -p pipe.log ]] || { echo "${PWD}/pipe.log is not a pipe"; exit 1; }
 exec >pipe.log 2>&1 /sbin/setuser nobody \
   ./app.sh
+  
