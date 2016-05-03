324fdf3d23e5810abd801f02ae2bfb4e585f4469
Elifarley C <elifarley@users.noreply.github.com>
2016-05-03 19:39:10 -0300

    Read environment name from "RAILS_ENV"

--- rails/puma.rb
+++ rails/puma.rb
@@ -3,6 +3,7 @@ workers ENV.fetch('PUMA_WORKERS', `nproc`).to_i
 max_threads = ENV.fetch('PUMA_MAX_THREADS', 16).to_i
 min_threads = ENV.fetch('PUMA_MIN_THREADS', max_threads).to_i
 threads min_threads, max_threads
+environment ENV.fetch('RAILS_ENV', 'development')
 
 bind 'tcp://0.0.0.0:8080'
 preload_app!
