24ea107487437178ff2395e2edc14e9f1457b521
Elifarley C <elifarley@users.noreply.github.com>
2016-06-16 17:14:36 -0300

    Send logs to stdout instead of /app/pipe.log

--- rails/app-service.sh
+++ rails/app-service.sh
@@ -2,7 +2,6 @@
 sv -w7 check syslog-ng
 cd /$_USER
 sleep 1
-[[ -p pipe.log ]] || { echo "${PWD}/pipe.log is not a pipe"; exit 1; }
-exec >pipe.log 2>&1 /sbin/setuser nobody \
+exec 2>&1 /sbin/setuser nobody \
   ./app.sh
   
